986a1bf16b123d075982b3b4cc1f2ce892d4a446
Ewerton Bortolozo Nunes Júnior <ewertonbortolozo2009@gmail.com>
2021-11-04 15:42:30 -0300

    Desafio 1 Feito
    Utilizando JS básico e html

---
+++ Desafios/Desafio_1/Desafio_1.js
@@ -0,0 +1,9 @@
+function botao1(){
+    window.alert("Você clicou no 1º Botão!")
+}
+function botao2(){
+    window.alert("Você clicou no 2º Botão!")
+}
+function botao3(){
+    window.alert("Você clicou no 3º Botão!")
+}
